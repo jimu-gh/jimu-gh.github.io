0fa919ac614be057bb6ccfe064deaeb2d514fb69
zer8ne <jimothy@gmail.com>
2017-05-20 21:38:56 -0700

    skills section added. work moved down to another section. next is projects.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,8 @@
 (function($){
   $(function(){
 
+    // Set Navbar according to device
+
     if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
         $('.topicon').hide();
         $('#dropdown1').html("");
@@ -11,6 +13,8 @@
         $('.topicon').show();
     }
 
+    // Scroll jump from dropdown
+
     $(".jumper").on("click", function( e ) {
         e.preventDefault();
         $("html, body").animate({
@@ -18,40 +22,35 @@
         }, 600);
     });
 
-    // CURRENT TIME
+    // Current time
     var currentDate = new Date();
     var currentHour = currentDate.getHours();
     if (currentHour < 3) {
         currentHour = 24;
     }
-
     var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var currentDay = currentDate.getDate();
     var currentMonthIndex = currentDate.getMonth();
     var currentYear = currentDate.getFullYear();
-
     var today = monthNames[currentMonthIndex] + " " + currentYear
     $("#today").text(today);
 
-    // SET BACKGROUND TO CURRENT TIME
+    // Set background to current time
     function setCurrentBackground(){
         if (currentHour >= 18 || currentHour < 6) {
             TextToWhite()
         }
-        // LUMP IN 3'S
         currentHour = currentHour - (currentHour % 3)
         var imgString = "images/" + Math.floor(currentHour) + "_lg.jpg"
         $('#bg1').attr("src", imgString);
     }
     setCurrentBackground();
 
-    // Initialize collapse button
+    // Initialize collapse button and parallax
     $(".button-collapse").sideNav();
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    //$('.collapsible').collapsible();
     $('.parallax').parallax();
 
-    // SET CUSTOM GREETING
+    // Custom greeting based on time
     var haveANiceDayStr = ""
     if (currentHour > 4 && currentHour <= 10) {
         haveANiceDayStr = "Good morning!^5000";
@@ -63,7 +62,7 @@
         haveANiceDayStr = "Good evening!^5000";
     }
 
-    // GET CURRENT TIME FROM BG
+    // Get current bg time for switch
     function getBGTime(){
         if ($('#bg1').attr("src")[8] == "_"){
             return parseInt($('#bg1').attr("src")[7]);
@@ -72,7 +71,7 @@
             return parseInt($('#bg1').attr("src")[7]+$('#bg1').attr("src")[8]);
         }
     }
-
+    // Text color flip
     function TextToWhite(){
         $('#parallaxbg').removeClass('white');
         $('#parallaxbg').addClass('black');
@@ -83,7 +82,6 @@
         $('#cursorobj').removeClass('black-text');
         $('#cursorobj').addClass('white-text');
     }
-
     function TextToBlack(){
         $('#parallaxbg').removeClass('black');
         $('#parallaxbg').addClass('white');
@@ -95,6 +93,7 @@
         $('#cursorobj').addClass('black-text');
     }
 
+    // Switch to next bg in regular intervals
     function nextBackground(){
         var hour = getBGTime();
         hour += 3;
@@ -116,16 +115,13 @@
             });
         })
     }
-
-    // REGULARLY UPDATE BACKGROUND
     function updateBackground(){
         nextBackground();
         setTimeout(updateBackground, 30000);
     }
-
     setTimeout(updateBackground, 30000);
 
-    // MESSAGE PROGRAMMING
+    // Message programming
     $("#topmsg").typed({
         strings: ["^250",haveANiceDayStr,"I am Jim Ho, ^1000000"],
         typeSpeed: 100,
@@ -146,22 +142,20 @@
         backDelay: 3000,
     });
 
-    // DROPDOWN SETTINGS
-
+    // Dropdown menu settings
     $('.dropdown-button').dropdown({
       inDuration: 300,
       outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
+      constrainWidth: true,
+      hover: true,
+      gutter: 0,
+      belowOrigin: true,
+      alignment: 'left',
+      stopPropagation: false
       }
     );
 
-    // ANIMATIONS
-
+    // Enable animations via jquery
     $.fn.extend({
         animateCss: function (animationName) {
 
@@ -173,57 +167,40 @@
 
     $('#topmsg').animateCss('rubberBand');
 
+    //For figuring out scroll values (optional)
     $(window).scroll(function(){
     var winScroll = $(this).scrollTop();
     console.log(winScroll);
-
-    // if(winScroll > $('.gallery_images').offset().top - ($(window).height()/1.2)){
-    //     $('.gallery_images figure').each(function(i){
-    //         setTimeout(function(){
-    //             $('.gallery_images figure').eq(i).addClass('is-showing');
-    //         }, 150 * (i+1));
-    //     });
-    // }
-
-         // if (winScroll > $('.gallery_images').offset().top - ($(window).height()/1.2)){
-          //   $('.gallery_images figure').each(function(){
-          //     setTimeout(function(i){
-          //       $('.gallery_images figure').eq(i).addClass('is-showing');
-          //     }, 150 * (i+1));
-          //   });
-          // }
     });
 
+    // Hover UI
     $('#email').hover(function(){
         $('#email').attr('src','images/icon_email_hover.png');
     },function(){
         $('#email').attr('src','images/icon_email.png');
     });
-
     $('#linkedin').hover(function(){
         $('#linkedin').attr('src','images/icon_linkedin_hover.png');
     },function(){
         $('#linkedin').attr('src','images/icon_linkedin.png');
     });
-
     $('#github').hover(function(){
         $('#github').attr('src','images/icon_github_hover.png');
     },function(){
         $('#github').attr('src','images/icon_github.png');
     });
-
     $('#resume').hover(function(){
         $('#resume').attr('src','images/icon_resume_hover.png');
     },function(){
         $('#resume').attr('src','images/icon_resume.png');
     });
-
     $('#instagram').hover(function(){
         $('#instagram').attr('src','images/icon_instagram_hover.png');
     },function(){
         $('#instagram').attr('src','images/icon_instagram.png');
     });
 
+    // About animations
     var wait1 = false;
     var wait2 = false;
     var currentStrength = "";
@@ -252,7 +229,7 @@
         'CLIENT SERVICE':["blue-text","tada"],
         'DETAIL':["teal-text","shake"],
         'EFFICIENCY':["green-text","jello"],
-        'EMPATHY':["pink-text","bounce"],
+        'EMPATHY':["pink-text","tada"],
         'LEADERSHIP':["grey-text","wobble"],
         'LOGIC':["red-text","shake"],
         'TEAMWORK':["purple-text","rubberBand"],
@@ -302,6 +279,16 @@
         $(this).addClass('strength');
         $(this).addClass('black-text');
     });
+
+    // Scrollfire customization
+    var options = [
+        {selector: '#skills1', offset: 200, callback: function(el) {
+            Materialize.showStaggeredList($('#skills1'));
+            setTimeout("Materialize.showStaggeredList($('#skills2'))",500);
+            setTimeout("Materialize.showStaggeredList($('#skills3'))",1000);
+        }}
+    ];
+    Materialize.scrollFire(options);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
